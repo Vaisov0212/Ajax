9d77ce584e00e4e199ac6d923da7cfb94ed4b842
vaisov0212 <karvak117@mail.ru>
2021-11-29 20:15:05 +0500

    instal Laravel^7 connect MySQL db success

---
+++ app/Student.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Student extends Model
+{
+    public $table='students';
+    public $fillable=[
+        'name',
+        'group'
+    ];
+}
